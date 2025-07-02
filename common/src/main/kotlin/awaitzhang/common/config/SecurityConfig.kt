64417938feea7cbfe31055a1e008f88a1292c4bd
白菜日记 <AwaitZhang@gmail.com>
2025-07-03 00:18:02 +0800

    feat(security): 启用 Spring Security 并配置基本安全性
    - 在 application.properties 中移除了 spring.security.enabled=false 配置
    - 在 common/build.gradle 中添加了 spring-boot-starter-security 依赖
    - 更新了 SecurityConfig.kt，启用了 Spring Security 并配置了基本的请求授权和 CSRF 保护

--- common/src/main/kotlin/awaitzhang/common/config/SecurityConfig.kt
+++ common/src/main/kotlin/awaitzhang/common/config/SecurityConfig.kt
@@ -1,31 +1,31 @@
-//package awaitzhang.common.config
-//
-//import org.springframework.context.annotation.Bean
-//import org.springframework.context.annotation.Configuration
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-//import org.springframework.security.config.http.SessionCreationPolicy
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-//import org.springframework.security.crypto.password.PasswordEncoder
-//import org.springframework.security.web.SecurityFilterChain
-//import org.springframework.security.config.annotation.web.invoke
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-//
-//@Configuration
-//@EnableWebSecurity
-//class SecurityConfig {
-//    @Bean
-//    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
-//
-//
-//    @Bean
-//    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-//        http {
-//            authorizeHttpRequests {
-//                authorize(anyRequest, permitAll) // 允许所有请求
-//            }
-//            csrf { disable() } // 禁用CSRF保护
-//        }
-//        return http.build()
-//    }
-//}
+package awaitzhang.common.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http {
+            authorizeHttpRequests {
+                authorize(anyRequest, permitAll) // 允许所有请求
+            }
+            csrf { disable() } // 禁用CSRF保护
+        }
+        return http.build()
+    }
+}
